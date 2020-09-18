9764ff97ab1f403b479c012abfa6aff79119d6e4
Timothy <timophobic@gmail.com>
2020-09-17 20:22:01 -0700

    Wordpress

--- code/gridfunction.mjs
+++
@@ -1,9 +0,0 @@
-var grid = function(x,y,width,height,xSpace,ySpace){
-    rect(x,y,width,height);
-    for(var i = 0 ; i<width; i+=xSpace){
-        line(x+i,y,x+i,y+height);
-    }
-    for(var i = 0 ; i<height; i+=ySpace){
-        line(x,y+i,x+width,y+i);
-    }
-};
